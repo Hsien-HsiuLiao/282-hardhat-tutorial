a51b37f8fa48b97a5fb209c9bab86e33692ec277
Hsien <hsien-hsiu.liao@yandex.com>
2024-07-11 10:13:22 -0700

    for react polyfill error

---
+++ frontend/config-overrides.js
@@ -0,0 +1,27 @@
+//from https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
+// as per https://github.com/ChainSafe/web3.js
+
+
+
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify"),
+        "url": require.resolve("url")
+    })
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ])
+    return config;
+}
