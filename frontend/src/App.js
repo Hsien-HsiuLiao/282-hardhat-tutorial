5ef68488cb426829f0005b399111905703915cd2
Hsien <hsien-hsiu.liao@yandex.com>
2024-07-11 12:41:18 -0700

    add code for DAO

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,363 @@
-import React, { useEffect, useState } from "react";
-import getBlockchain from './ethereum.js';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+//import DAO from './contract/DAOweb3artifact.json';
+import DAO from './DAOweb3.json';
+
+import { getWeb3 } from './utils.js';
+//import getBlockchain from './ethereum.js';
+
 
 function App() {
-  const [token, setToken] = useState(undefined);
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [admin, setAdmin] = useState(undefined);
+  const [shares, setShares] = useState(undefined);
+  const [proposals, setProposals] = useState([]);
+  const [isInvestor, setInvestorStatus] = useState(undefined);
 
   useEffect(() => {
     const init = async () => {
-      const { token } = await getBlockchain();
-      setToken(token);
-    };
+      //const web3 = await getWeb3();
+      const web3 = new Web3("ws://localhost:8545")
+      // const web3 = await getBlockchain();
+      console.log(web3);
+      //let [signerAddress, DAOcontract] = web3;
+      // console.log('signerAddress, DAOcontract', signerAddress, DAOcontract);
+
+      const accounts = await web3.eth.getAccounts();
+      console.log('accounts', accounts);
+
+      const networkId = await web3.eth.net.getId();
+      //console.log('DAO', DAO);
+      //const deployedNetwork = DAO.networks[networkId];
+
+      //DAO address: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+      const daoAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+      console.log('DAO.abi', DAO.abi);
+      const contract = new web3.eth.Contract(
+        DAO.abi,
+        //daoAddress
+        DAO.address
+        //   deployedNetwork && deployedNetwork.address,
+      );
+      // const contract = web3.DAOcontract;
+
+      console.log('contract', contract.methods.admin);
+
+      const admin = await contract
+        .methods
+        .admin()
+        .call();
+
+
+
+      setWeb3(web3);
+      console.log('set');
+      setAccounts(accounts);
+      setContract(contract);
+      setAdmin(admin);
+
+      
+
+      const isInvestor = await contract.methods.investors(accounts[0]).call();
+      setInvestorStatus(isInvestor);
+      console.log("status init: ", isInvestor);
+      //find out if current account, accounts[0] is investor
+      // await contract.methods.investors(accounts[0]).call() =true?
+    }
     init();
+    window.ethereum.on('accountsChanged', accounts => {
+      setAccounts(accounts);
+    });
   }, []);
 
-  if(
-    typeof token === 'undefined'
-  ) {
-    return 'Loading...';
+  const isReady = () => {
+    return (
+      typeof contract !== 'undefined'
+      && typeof web3 !== 'undefined'
+      && typeof accounts !== 'undefined'
+      && typeof admin !== 'undefined'
+    );
+  }
+
+  useEffect(() => {
+    if (isReady()) {
+      updateShares();
+      updateProposals();
+    }
+  }, [accounts, contract, web3, admin, isInvestor]);
+
+  async function updateShares() {
+    console.log("updateShares() called:", accounts[0]);
+    const shares = await contract.methods
+      .shares(accounts[0])
+      .call();
+    setShares(shares);
+    //setInvestorStatus
+    const isInvestor = await contract.methods.investors(accounts[0]).call();
+    console.log("isInvestor: ", isInvestor);
+    setInvestorStatus(isInvestor);
+  }
+
+  async function updateProposals() {
+    const nextProposalId = parseInt(await contract.methods
+      .nextProposalId()
+      .call());
+
+    const proposals = [];
+    for (let i = 0; i < nextProposalId; i++) {
+      const [proposal, hasVoted] = await Promise.all([
+        contract.methods.proposals(i).call(),
+        contract.methods.votes(accounts[0], i).call()
+      ]);
+      proposals.push({ ...proposal, hasVoted });
+    }
+    setProposals(proposals);
+  }
+
+  async function executeProposal(proposalId) {
+    await contract.methods
+      .executeProposal(proposalId)
+      .send({ from: accounts[0] });
+    await updateProposals();
+  };
+
+  async function withdrawEther(e) {
+    e.preventDefault();
+    const amount = e.target.elements[0].value;
+    const to = e.target.elements[1].value;
+    await contract.methods
+      .withdrawEther(amount, to)
+      .send({ from: accounts[0] });
+  };
+
+  async function contribute(e) {
+    e.preventDefault();
+    const amount = e.target.elements[0].value;
+    await contract.methods
+      .contribute()
+      .send({ from: accounts[0], value: amount });
+    await updateShares();
+    // const isInvestor = await contract.methods.investors(accounts[0]).call();
+    //  console.log("isInvestor: ", isInvestor);
+    // setInvestorStatus(isInvestor);
+  };
+
+  async function redeemShares(e) {
+    e.preventDefault();
+    const amount = e.target.elements[0].value;
+    await contract.methods
+      .redeemShare(amount)
+      .send({ from: accounts[0] });
+    await updateShares();
+  };
+
+  async function transferShares(e) {
+    e.preventDefault();
+    const amount = e.target.elements[0].value;
+    const to = e.target.elements[1].value;
+    await contract.methods
+      .transferShare(amount, to)
+      .send({ from: accounts[0] });
+    await updateShares();
+  };
+
+  async function vote(ballotId) {
+    await contract.methods
+      .vote(ballotId)
+      .send({ from: accounts[0] });
+    await updateProposals();
+  };
+
+  async function createProposal(e) {
+    e.preventDefault();
+    const name = e.target.elements[0].value;
+    const amount = e.target.elements[1].value;
+    const recipient = e.target.elements[2].value;
+    await contract.methods
+      .createProposal(name, amount, recipient)
+      .send({ from: accounts[0] });
+    await updateProposals();
+  };
+
+  function isFinished(proposal) {
+    const now = new Date().getTime();
+    const proposalEnd = new Date(parseInt(proposal.end) * 1000);
+    return (proposalEnd > now) > 0 ? false : true;
+  }
+
+  if (!isReady()) {
+    return <div>Loading...</div>;
   }
 
   return (
-    <div className="App">
-    app loaded
+    <div className="container">
+      <h1 className="text-center">DAO</h1>
+      <h4>current account address: {accounts}</h4>
+      <h4>Investor status: {String(isInvestor)}</h4>
+      <p>Shares: {shares}</p>
+
+      {accounts[0].toLowerCase() === admin.toLowerCase() ? (
+        <>
+          <div className="row">
+            <div className="col-sm-12">
+              <h2>Withdraw ether</h2>
+              <form onSubmit={e => withdrawEther(e)}>
+                <div className="form-group">
+                  <label htmlFor="amount">Amount</label>
+                  <input type="text" className="form-control" id="amount" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="to">To</label>
+                  <input type="text" className="form-control" id="to" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+          <hr />
+        </>
+      ) : null}
+
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>Contribute - (become an investor)</h2>
+          <form onSubmit={e => contribute(e)}>
+            <div className="form-group">
+              <label htmlFor="amount">Amount</label>
+              <input type="text" className="form-control" id="amount" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <hr />
+      {/* only display if account is investor
+ html code 
+*/ }
+      {isInvestor ? (
+        <>
+          <div className="row">
+            <div className="col-sm-12">
+              <h2>Redeem shares</h2>
+              <form onSubmit={e => redeemShares(e)}>
+                <div className="form-group">
+                  <label htmlFor="amount">Amount</label>
+                  <input type="text" className="form-control" id="amount" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+
+          <hr />
+
+        </>
+      ) : null}
+
+      {/* only display if account is investor*/}
+
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>Transfer shares</h2>
+          <form onSubmit={e => transferShares(e)}>
+            <div className="form-group">
+              <label htmlFor="amount">Amount</label>
+              <input type="text" className="form-control" id="amount" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="to">To</label>
+              <input type="text" className="form-control" id="to" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <hr />
+      {/* only display if account is investor*/}
+
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>Create proposal</h2>
+          <form onSubmit={e => createProposal(e)}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="amount">Amount</label>
+              <input type="text" className="form-control" id="amount" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="recipient">Recipient</label>
+              <input type="text" className="form-control" id="recipient" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>Proposals</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Recipient</th>
+                <th>Votes</th>
+                <th>Vote</th>
+                <th>Ends on</th>
+                <th>Executed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {proposals.map(proposal => (
+                <tr key={proposal.id}>
+                  <td>{proposal.id}</td>
+                  <td>{proposal.name}</td>
+                  <td>{proposal.amount}</td>
+                  <td>{proposal.recipient}</td>
+                  <td>{proposal.votes}</td>
+                  <td>
+                    {isFinished(proposal) ? 'Vote finished' : (
+                      proposal.hasVoted ? 'You already voted' : (
+                        <button
+                          onClick={e => vote(proposal.id)}
+                          type="submit"
+                          className="btn btn-primary">
+                          Vote
+                      </button>
+                      ))}
+                  </td>
+                  <td>
+                    {(new Date(parseInt(proposal.end) * 1000)).toLocaleString()}
+                  </td>
+                  <td>
+                    {proposal.executed ? 'Yes' : (
+                      admin.toLowerCase() === accounts[0].toLowerCase() ? (
+                        <button
+                          onClick={e => executeProposal(proposal.id)}
+                          type="submit"
+                          className="btn btn-primary">
+                          Execute
+                        </button>
+                      ) : 'No'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
